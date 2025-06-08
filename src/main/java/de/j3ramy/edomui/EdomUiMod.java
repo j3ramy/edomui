f220a937e2d112a87dcc25c4035cca8d9c948155
j3ramy <jeramy.gaming@gmail.com>
2025-06-08 20:35:58 +0200

    Initial commit

---
+++ src/main/java/de/j3ramy/edomui/EdomUiMod.java
@@ -0,0 +1,35 @@
+package de.j3ramy.edomui;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import static de.j3ramy.edomui.EdomUiMod.MOD_ID;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MOD_ID)
+public class EdomUiMod
+{
+    public static final String MOD_ID = "edomui";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public EdomUiMod()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
