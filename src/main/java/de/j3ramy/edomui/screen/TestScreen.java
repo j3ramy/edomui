2eb1b1bbc8dab6c79ed28f0fabc25464387e5b16
j3ramy <jeramy.gaming@gmail.com>
2025-07-04 19:50:39 +0200

    Remove widgets from test screen

--- src/main/java/de/j3ramy/edomui/screen/TestScreen.java
+++ src/main/java/de/j3ramy/edomui/screen/TestScreen.java
@@ -1,8 +1,5 @@
 package de.j3ramy.edomui.screen;
 
-import de.j3ramy.edomui.component.chart.BarChart;
-import de.j3ramy.edomui.component.chart.LineChart;
-import de.j3ramy.edomui.util.chart.DataPoint;
 import de.j3ramy.edomui.view.View;
 import de.j3ramy.edomui.view.ViewRegistry;
 import de.j3ramy.edomui.enums.FontSize;
@@ -11,7 +8,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.TextComponent;
 
 import java.awt.*;
-import java.util.List;
 
 public class TestScreen extends Screen {
     private View view;
@@ -26,13 +22,7 @@ public class TestScreen extends Screen {
 
         ViewRegistry.registerView(this.view = new View());
 
-        this.view.addWidget(new Text(10, 10, "Hey there! Greetings from EdomUI", FontSize.BASE, Color.BLUE));
-
-        LineChart barChart = new LineChart(5, 10, 120, 80);
-        barChart.getStyle().setLineColor(Color.MAGENTA);
-        view.addWidget(barChart);
-        barChart.addDataPoints(List.of(new DataPoint("Ausweis", 20),
-                new DataPoint("Baugenehmigung", 9), new DataPoint("Bier", 15)));
+        this.view.addWidget(new Text(10, 10, "Hey! EdomUI is working :D", FontSize.XS, Color.BLUE));
     }
 
     @Override
