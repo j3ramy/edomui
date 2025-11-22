d8b24b7ca037fb04ca10f115ad05001f25be6f83
j3ramy <jeramy.gaming@gmail.com>
2025-11-22 19:50:12 +0100

    Set max NUMBER_FIELD_CHAR_LIMIT because of string formatting error

--- src/main/java/de/j3ramy/edomui/util/style/GuiPresets.java
+++ src/main/java/de/j3ramy/edomui/util/style/GuiPresets.java
@@ -7,7 +7,7 @@ public class GuiPresets {
     public static int DOUBLE_CLICK_THRESHOLD_FAST = 250; // TextField
     public static int DOUBLE_CLICK_THRESHOLD_NORMAL = 500; // TextArea
     public static int TEXT_FIELD_CHAR_LIMIT = 256;
-    public static int NUMBER_FIELD_CHAR_LIMIT = 10;
+    public static int NUMBER_FIELD_CHAR_LIMIT = 7;
     public static int TEXT_AREA_CHAR_LIMIT = 131072;
     public static String TEXT_AREA_DELIMITER = "\n";
     public static float MAX_PROGRESS_BAR_VALUE = 1;
@@ -42,6 +42,10 @@ public class GuiPresets {
     }
 
     public static void setNumberFieldCharLimit(int numberFieldCharLimit) {
+        if(numberFieldCharLimit > 7){
+            return;
+        }
+
         NUMBER_FIELD_CHAR_LIMIT = numberFieldCharLimit;
     }
 
